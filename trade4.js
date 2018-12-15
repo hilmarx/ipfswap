103b9c1b4f1777ca9edcfbfab1711990ee2a9173
Hilmar Orth <hm.orth@aol.com>
2018-12-15 17:17:54 +0100

    ignore changes

--- trade4.js
+++ trade4.js
@@ -2,14 +2,13 @@
 
 function test() {
 
-web3.eth.sendTransaction({
-    from: '0xAC5781f6eEC1f0Cb9B353aCBB745f813cEf68311',
-    to: '0xb779bEa600c94D0a2337A6A1ccd99ac1a8f08866',
-    value: '1000000000000000',
-    gasPrice: '0x09184e72a000',
-    gasLimit: '0x2710'
-})
-
+  web3.eth.sendTransaction({
+      from: '0xAC5781f6eEC1f0Cb9B353aCBB745f813cEf68311',
+      to: '0xb779bEa600c94D0a2337A6A1ccd99ac1a8f08866',
+      value: '1000000000000000',
+      gasPrice: '0x09184e72a000',
+      gasLimit: '0x2710'
+  })
 }
 
 test();
