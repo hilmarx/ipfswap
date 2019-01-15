d58578418ee52799fe048838947980115a22f83c
Hilmar Orth <hm.orth@aol.com>
2019-01-15 21:55:56 +0100

    refactored Modal Pop Ups

--- javascript/trade5.js
+++ javascript/trade5.js
@@ -112,6 +112,120 @@ let successful;
 // Counter to protect users from creating two event listeners on the swap button that will result in 2 tx's to be signed
 let counter = 0
 
+
+// async function test() {
+
+//   srcAmountWei = `${srcAmount * (10 ** 18)}`;
+
+//     async function test1() {
+//       txReceipt = await web3.eth.sendTransaction({
+//                     from: fetchedUserAddress, //obtained from website interface Eg. Metamask, Ledger etc.
+//                     to: kyberNetworkProxyAddress,
+//                     data: transactionData,
+//                     }).catch(function(error) {
+//                       console.log(error);
+//                       loaderToSwap();
+//                     })
+
+//     }
+
+//   //First, user must approve KyberNetwork contract to trade src tokens
+//   srcTokenContract = new web3.eth.Contract(ERC20ABI, addressToSell);
+
+//   transactionData1 = srcTokenContract.methods.approve(kyberNetworkProxyAddress, srcAmountWei).encodeABI()
+
+
+//   txReceipt = await web3.eth.sendTransaction({
+//             from: fetchedUserAddress, //obtained from website interface Eg. Metamask, Ledger etc.
+//             to: addressToSell, //srcTokenContract resluted in error as it did not provide the contracts address, but the object itself,
+//             data: transactionData1
+//             }).on('transactionHash', function(hash){
+//               transactionData = kyberNetworkProxyContract.methods.trade(
+//               addressToSell, //ERC20 srcToken
+//               srcAmountWei, //uint srcAmount
+//               addressToBuy, //ERC20 destToken
+//               fetchedUserAddress, //address destAddress => VENDOR_WALLET_ADDRESS
+//               "10000000000000000000000000000000", //uint maxDestAmount
+//               slippageRate, //uint minConversionRate
+//               "0xb779bEa600c94D0a2337A6A1ccd99ac1a8f08866" //uint walletId
+//             ).encodeABI()
+
+//             test1()
+
+//             });
+// }
+
+
+//  ##################### Modal changes #######################
+function startModal() {
+
+  if (addressToSell == "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee") {
+    // Display Modal for a successful swap
+    modalTitle.innerText = "Please approve the Swap 🤖";
+    closeBtn.innerText = "Approve"
+  } else {
+    modalTitle.innerText = "Please execute the Swap 🤖";
+    closeBtn.innerText = "Confirm"
+  }
+
+  modalBody.innerHTML = `<div id="confirm-text">${srcAmountWei / srcQuantity} ${srcSymbol} for ${(srcAmount  * expectedRate) / srcQuantity} ${destSymbol}\n</div> <div id="slippage-note">A max 3% slippage Rate may be applied in situations of larger market movements during trade execution.</div>` ;
+
+  modalBody.style.display = "";
+
+  closeBtn.innerText = "Approve"
+  $('.modal').modal('show');
+
+}
+
+function tradeApprovedModal() {
+  // Alert modal to ask for confirmation of approved transaction
+  modalTitle.innerText = "Now confirm the approved Swap to exchange the tokens";
+  closeBtn.innerText = "Confirm"
+  modalBody.innerHTML = ``
+  modalBody.style.display = "none"
+  $('.modal').modal('show');
+
+}
+
+function waitingModal(hash) {
+   // Change Swap Button for loader
+  swapToLoader();
+  // Change Modal to say please wait
+  modalTitle.innerText = "Please wait for the transaction to be mined 🕒";
+  modalBody.innerHTML = ``;
+  modalBody.innerText = `Meanwhile, you can check the tx status on Etherscan`;
+  modalBody.style.display = "";
+  closeBtn.style.display = "none";
+  metaMaskBtn.innerText = "Check Tx Status";
+  (selectedEthereumNetwork == "mainnet") ? etherscanUrl = `https://etherscan.io/tx/${hash}` : etherscanUrl = `https://ropsten.etherscan.io/tx/${hash}`
+  metaMaskBtn.href = etherscanUrl
+  metaMaskBtn.style.display = "";
+  $('.modal').modal('show');
+
+}
+
+function completedModal() {
+  // Change Loader for Swap Button
+  loaderToSwap();
+
+  // Display Modal for a successful swap
+  modalTitle.innerText = "Swap successful 👍";
+  modalBody.innerText = "";
+  modalBody.style.display = "none"
+  // Re-display close button
+  closeBtn.innerText = "New Swap"
+  closeBtn.style.display = ""
+  closeBtn.removeEventListener("click", executeTx, { passive: true });
+  closeBtn.addEventListener("click", reloadMainPage);
+  metaMaskBtn.style.display = "none";
+  $('.modal').modal('show');
+}
+
+//  ##################### Modal changes END #######################
+
+
+
+
 // Let the trade begin
 
 async function trade() {
@@ -140,13 +254,20 @@ async function trade() {
     if (etherBalance >= parseInt(srcAmountWei) ) {
 
       // Add Event listener to "SWAP" button of Modal which when clicked open the transaction
-
       // Display Modal for a successful swap
       modalTitle.innerText = "Please confirm the Swap 🤖";
       modalBody.innerHTML = `<div id="confirm-text">${srcAmountWei / srcQuantity} ${srcSymbol} for ${(srcAmount  * expectedRate) / srcQuantity} ${destSymbol}\n</div> <div id="slippage-note">A max 3% slippage Rate may be applied in situations of larger market movements during trade execution.</div>`
       modalBody.style.display = "";
       $('.modal').modal('show');
       closeBtn.innerText = "Confirm";
+
+            // Display Modal for a successful swap
+      modalTitle.innerText = "Please approve the Swap 🤖";
+      // modalBody.innerText = `${srcAmountWei / srcQuantity} ${srcSymbol} for ${(srcAmount  * expectedRate) / srcQuantity} ${destSymbol}\n`
+      modalBody.innerHTML = `<div id="confirm-text">${srcAmountWei / srcQuantity} ${srcSymbol} for ${(srcAmount  * expectedRate) / srcQuantity} ${destSymbol}\n</div> <div id="slippage-note">A max 3% slippage Rate may be applied in situations of larger market movements during trade execution.</div>` ;
+      modalBody.style.display = "";
+      closeBtn.innerText = "Approve"
+      $('.modal').modal('show');
 
       async function executeTx() {
 
@@ -207,6 +328,7 @@ async function trade() {
         closeBtn.addEventListener("click", reloadMainPage);
         metaMaskBtn.style.display = "none";
         $('.modal').modal('show');
+
       }
       counter += 1;
       if(counter < 2) closeBtn.addEventListener('click', executeTx);
@@ -228,26 +350,21 @@ async function trade() {
     //First, user must approve KyberNetwork contract to trade src tokens
     srcTokenContract = new web3.eth.Contract(ERC20ABI, addressToSell);
 
+    // Check if User gave Kyber any allowance in order to skip allow tx
+    allowanceAmount = await srcTokenContract.methods.allowance(fetchedUserAddress,kyberNetworkProxyAddress).call()
+
+    if (srcAmountWei <= allowanceAmount) {
+        //proceed to step 3
+        console.log(`Source Amount: ${srcAmountWei} is smaller than AllowanceAmount ${allowanceAmount}`)
+    } else {
+        //proceed to step 2
+        console.log(`Source Amount: ${srcAmountWei} is greater than AllowanceAmount ${allowanceAmount}`)
+    }
+
     // Call balanceOf function
     let erc20Balance = await srcTokenContract.methods.balanceOf(fetchedUserAddress).call();
-    console.log(erc20Balance)
-    console.log(srcAmountWei)
 
     if (erc20Balance >= parseInt(srcAmountWei) ) {
-
-      transactionData1 = srcTokenContract.methods.approve(kyberNetworkProxyAddress, srcAmountWei).encodeABI()
-
-      // ####### Start second tx ########
-      // Call the trade method in Proxy Contract
-      transactionData2 = kyberNetworkProxyContract.methods.trade(
-        addressToSell, //ERC20 srcToken
-        srcAmountWei, //uint srcAmount
-        addressToBuy, //ERC20 destToken
-        fetchedUserAddress, //address destAddress => VENDOR_WALLET_ADDRESS
-        "10000000000000000000000000000000", //uint maxDestAmount
-        slippageRate, //uint minConversionRate
-        walletId //uint walletId for fee sharing program
-      ).encodeABI()
 
       // Display Modal for a successful swap
       modalTitle.innerText = "Please approve the Swap 🤖";
@@ -258,6 +375,9 @@ async function trade() {
       $('.modal').modal('show');
 
       async function approveTx() {
+
+        transactionData1 = srcTokenContract.methods.approve(kyberNetworkProxyAddress, srcAmountWei).encodeABI()
+
 
         txReceipt = await web3.eth.sendTransaction({
             from: fetchedUserAddress, //obtained from website interface Eg. Metamask, Ledger etc.
@@ -274,7 +394,19 @@ async function trade() {
               modalBody.style.display = "none"
               $('.modal').modal('show');
 
-              async function executeTrade() {
+              async function executeTx() {
+
+              // ####### Start second tx ########
+              // Call the trade method in Proxy Contract
+              transactionData2 = kyberNetworkProxyContract.methods.trade(
+                addressToSell, //ERC20 srcToken
+                srcAmountWei, //uint srcAmount
+                addressToBuy, //ERC20 destToken
+                fetchedUserAddress, //address destAddress => VENDOR_WALLET_ADDRESS
+                "10000000000000000000000000000000", //uint maxDestAmount
+                slippageRate, //uint minConversionRate
+                walletId //uint walletId for fee sharing program
+              ).encodeABI()
 
               txReceipt = await web3.eth.sendTransaction({
                 from: fetchedUserAddress, //obtained from website interface Eg. Metamask, Ledger etc.
