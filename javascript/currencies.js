ecb320bb9c64b5e743cd6ba02feaf95a21351028
Daniell Mesquita <daniellmesquita@protonmail.com>
2019-11-05 00:08:58 -0200

    Token icons from GitLab's CommonDatabase
    Signed-off-by: Daniell Mesquita <daniellmesquita@protonmail.com>

--- javascript/currencies.js
+++ javascript/currencies.js
@@ -70,7 +70,7 @@ function fetchApiFromSelectedNetwork() {
     // Kyber Network ERC20 mainnet address
     addressToBuy = "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359";
   } else if (selectedEthereumNetwork = "ropsten" ) {
-    // Kyber Network ERC20 ropstem address
+    // Kyber Network ERC20 ropsten address
     addressToBuy = "0xad6d458402f60fd3bd25163575031acdce07538d";
   };
 }
@@ -211,7 +211,7 @@ function setBuyValues() {
   let buyTokenImageUrl = mainnetAddresses[destSymbol];
 
   // Set token-logo for token to buy. If it is ETH, then use local image
-  (destSymbol == "ETH") ? buyLogo.src = "images/ethereum.png" : buyLogo.src = `https://raw.githubusercontent.com/TrustWallet/tokens/master/tokens/${buyTokenImageUrl}.png`;
+  (destSymbol == "ETH") ? buyLogo.src = "images/ethereum.png" : buyLogo.src = `https://gitlab.com/CommonDatabase/tokens/raw/master/img/${buyTokenImageUrl}.svg`;
 
   // Re-run getExpectedRate function for new address pair & wait for the promise to resolve. Then update the numbers in the dest field
   getExpectedRate()
@@ -232,7 +232,7 @@ function setSellValues() {
   let sellTokenImageUrl = mainnetAddresses[srcSymbol];
 
   // Set token-logo for token to sell. If it is ETH, then use local image
-  (srcSymbol == "ETH") ? sellLogo.src = "images/ethereum.png" : sellLogo.src = `https://raw.githubusercontent.com/TrustWallet/tokens/master/tokens/${sellTokenImageUrl}.png`;
+  (srcSymbol == "ETH") ? sellLogo.src = "images/ethereum.png" : sellLogo.src = `https://gitlab.com/CommonDatabase/tokens/raw/master/img/${sellTokenImageUrl}.svg`;
 
   // Calc srcQuantity with srcDecimal
   srcQuantity = `${10 ** srcDecimal}`;
